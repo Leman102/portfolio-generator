6b5e38ff8776f9bb163dd4ec1cbeec0aaf849a9c
Leoman <leo.pinzon102@gmail.com>
2022-06-27 17:35:05 -0400

    adding html file

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
+const fs = require('fs');
+//add generatePage function
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length); 
-//console.log(profileDataArgs)
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
+const [name, github] = profileDataArgs;
 
-// const printProfileData = (profileDataArr) => {
-//     console.log(profileDataArr);
-// };
-
-const printProfileData = profileDataArr => {
-    // for (let i = 0; i < profileDataArr.length; i++){
-    //     console.log(profileDataArr[i]);
-    // }
-    // profileDataArr.forEach((profileItem) => {
-    //     console.log(profileItem);        
-    // });
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs)
+//addidng fs writeFile module to create html, needs 3 arg
+// file name, data(function), error handling
+fs.writeFile('index.html',generatePage(name,github),err => {
+    if(err) throw err;
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
